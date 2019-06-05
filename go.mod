c5881f860c06caff00407214ab6667d6d5ebe6ab
Ivan Goncharov <igoncharov@openwaygroup.com>
2019-06-05 18:22:31 +0300

    ADD: working mock example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/imorph/string-unpacker
 
 go 1.12
+
+require github.com/imorph/string-unpacker/pkg/unpkr v0.0.0-20190605150840-cf74e4811b4e
--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"log"
 
-	"github.com/imorph/string-unpacker/pkg/unpkr"
+	u "github.com/imorph/string-unpacker/pkg/unpkr"
 )
 
 func main() {
-	fmt.Println("lets UNPACK!")
+	fmt.Println("")
+	fmt.Println("let's UNPACK!")
 	fmt.Println("Press any time CTRL-C to exit")
+	fmt.Println("")
 
-	var pkgString PackedString
+	var pkgString u.PackedString
 
 	for {
 		fmt.Print("Please enter a correct packed string: ")
 
-		_, err := fmt.Scanf("%d", &pkgString)
+		_, err := fmt.Scanf("%s", &pkgString)
 		if err != nil {
 			log.Println(err)
 		} else {
 			fmt.Println("Unpacked string: ", pkgString.Unpack())
+			fmt.Println("")
 		}
 		}
 	}
-
-}
